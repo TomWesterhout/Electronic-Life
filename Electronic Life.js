c5d012a42f199717a95e976a67f6041d4eca1850
TomWesterhout <westerhout_tom@hotmail.com>
2017-12-02 18:46:37 +0100

    Added describing comments to the created code.

--- Electronic Life.js
+++ Electronic Life.js
@@ -1,3 +1,4 @@
+// Represents coordinates used for example when requesting a square from a grid.
 function Vector(x, y) {
 	this.x = x;
 	this.y = y;
@@ -6,6 +7,7 @@ Vector.prototype.plus = function(other) {
 	return new Vector(this.x + other.x, this.y + other.y);
 };
 
+// Creates a Grid object which contains getter and setter properties for requesting or setting a square value in the corresponding array.
 function Grid(width, height) {
 	this.space = new Array(width * height);
 	this.width = width;
@@ -25,18 +27,46 @@ Grid.prototype.set = function(vector, value) {
 	this.space[vector.x + this.width * vector.y] = value;
 };
 
+// Returns a random element from an array.
 function randomElement(array) {
 	return array[Math.floor(Math.random() * array.length)];
 };
 
 var directionNames = "n ne e se s sw w nw".split(" ");
 
+// Creates a simple critter object.
 function BouncingCritter() {
 	this.direction = randomElement(directionNames);
 }
 
+// Returns an action object containing a action type and added information based on the results of the look and/or find methods.
 BouncingCritter.prototype.act = function (view) {
 	if (view.look(this.direction) != " ")
 		this.direction = view.find(" ") || "s";
 	return { type: "move", direction: this.direction };
 };
+
+// Returns a legend object which contains the meaning of the characters used in a map.
+function elementFromChar(legend, ch) {
+	if (ch == " ")
+		return null;
+	var element = new legend[ch]();
+	element.originChar = ch;
+	return element;
+}
+
+// Returns a World object which contains a Grid object and sets the Grid's space array.
+function World(map, legend) {
+	var grid = new Grid(map[0].length, map.length);
+	this.grid = grid;
+	this.legend = legend;
+
+
+}
+
+
+
+
+
+
+
